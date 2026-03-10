3c6c5375e12ae711fc73ab20b30b90ee26c55660
OMI <OMI@d.c>
2026-03-10 15:54:04 +0200

    done with model models

---
+++ ForkOverflow/src/ForkOverflow.Domain/Tags/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForkOverflow.Domain.Tags
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+    }
+}
